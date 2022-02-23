0d554bfe2b859c8f2b7e4a2e9674a8cc80e3d3aa
inoth <inoth.work@gmail.com>
2022-02-23 09:48:11 +0800

    .

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
 import (
-	"defaultProject/cache"
 	"defaultProject/config"
-	"defaultProject/db"
 	"defaultProject/global"
 	"defaultProject/httpsvc"
 	"defaultProject/logger"
-	"defaultProject/queue"
 	"defaultProject/src/router"
 )
 
@@ -15,9 +12,9 @@ func main() {
 	global.Register(
 		&config.ViperConfig{},
 		&logger.LogrusConfig{},
-		&cache.RedisCache{},
-		&db.MysqlConnect{},
-		&db.MongoConnect{},
-		&queue.NsqQueue{},
-	).Init().ServeStart(httpsvc.NewGinConfig().SetRouter(&router.ProjectRouter{}))
+		// &cache.RedisCache{},
+		// &db.MysqlConnect{},
+		// &db.MongoConnect{},
+		// &queue.NsqQueue{},
+	).Init().ServeStart(httpsvc.NewGinConfig().SetRouter(&router.ProjectRouter{}, &router.Project2Router{}))
 }
--- src/router/router.go
+++ src/router/router.go
@@ -6,6 +6,14 @@ type ProjectRouter struct{}
 
 func (ProjectRouter) Load(g *gin.Engine) {
 	g.GET("/", func(c *gin.Context) {
-		c.String(200, "hello world")
+		c.String(200, "hello world project")
+	})
+}
+
+type Project2Router struct{}
+
+func (Project2Router) Load(g *gin.Engine) {
+	g.GET("/v1", func(c *gin.Context) {
+		c.String(200, "hello world project2")
 	})
 }
